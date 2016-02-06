4b49aa2f0f25aa9e033f3539ea7d8fb8a6655e6c
Paolo Scanferla <paolo.scanferla@mondora.com>
2016-02-06 14:58:49 +0100

    Moved all react-router setup code into lib/routes.jsx

--- app/lib/routes.jsx
+++ app/lib/routes.jsx
@@ -1,11 +1,13 @@
 import React from "react";
-import {IndexRoute, Route} from "react-router";
+import {IndexRoute, Route, Router, hashHistory} from "react-router";
 
 import Root from "views/root";
 import Home from "views/home";
 
 export default (
-    <Route component={Root} path="/">
-        <IndexRoute component={Home} />
-    </Route>
+    <Router history={hashHistory}>
+        <Route component={Root} path="/">
+            <IndexRoute component={Home} />
+        </Route>
+    </Router>
 );
--- app/main.jsx
+++ app/main.jsx
@@ -2,16 +2,13 @@ import "babel-polyfill";
 import React from "react";
 import {render} from "react-dom";
 import {Provider} from "react-redux";
-import {Router, hashHistory} from "react-router";
 
 import routes from "lib/routes";
 import store from "lib/store";
 
 const App = (
     <Provider store={store}>
-        <Router history={hashHistory}>
-            {routes}
-        </Router>
+        {routes}
     </Provider>
 );
 
