8d45de96a1701ccd37da1956e5e1fb3c533e20e6
Paolo Scanferla <paolo.scanferla@mondora.com>
2016-02-06 12:57:06 +0100

    Added copy-to-clipboard tests

--- test/components/codes-list.jsx
+++ test/components/codes-list.jsx
@@ -3,10 +3,13 @@ import jsxChai from "jsx-chai";
 import {range, values} from "ramda";
 import React from "react";
 import TestUtils from "react-addons-test-utils";
+import sinon from "sinon";
+import sinonChai from "sinon-chai";
 
 import CodesList from "components/codes-list";
 
 chai.use(jsxChai);
+chai.use(sinonChai);
 
 describe("components/codes-list", () => {
 
@@ -26,6 +29,17 @@ describe("components/codes-list", () => {
         const component = TestUtils.renderIntoDocument(
             <CodesList codes={codes} />
         );
+        const API_URL = "http://shooooort.com";
+        const copy = sinon.spy();
+
+        before(() => {
+            CodesList.__Rewire__("API_URL", API_URL);
+            CodesList.__Rewire__("copy", copy);
+        });
+        after(() => {
+            CodesList.__ResetDependency__("API_URL");
+            CodesList.__ResetDependency__("copy");
+        });
 
         it("renders a table", () => {
             TestUtils.findRenderedDOMComponentWithTag(component, "table");
@@ -62,6 +76,38 @@ describe("components/codes-list", () => {
                 // Test their highlight bar is not visible (opacity === 0)
                 .forEach(td => {
                     expect(td.firstChild.firstChild.style.opacity).to.equal("0");
+                });
+        });
+
+        it("copies short link to clipboard when entry is clicked [CASE: click on tr]", () => {
+            TestUtils
+                // Get all rows
+                .scryRenderedDOMComponentsWithTag(component, "tr")
+                // Filter out head rows
+                .filter(tr => tr.parentElement.tagName !== "THEAD")
+                // Test click-to-copy
+                .forEach((tr, idx) => {
+                    copy.reset();
+                    TestUtils.Simulate.click(tr);
+                    expect(copy).to.have.callCount(1);
+                    expect(copy).to.have.been.calledWith(`http://shooooort.com/code-${idx}`);
+                });
+        });
+
+        it("copies short link to clipboard when entry is clicked [CASE: click on tr child]", () => {
+            TestUtils
+                // Get all rows
+                .scryRenderedDOMComponentsWithTag(component, "tr")
+                // Filter out head rows
+                .filter(tr => tr.parentElement.tagName !== "THEAD")
+                // Get child
+                .map(tr => tr.firstChild.firstChild)
+                // Test click-to-copy
+                .forEach((tr, idx) => {
+                    copy.reset();
+                    TestUtils.Simulate.click(tr);
+                    expect(copy).to.have.callCount(1);
+                    expect(copy).to.have.been.calledWith(`http://shooooort.com/code-${idx}`);
                 });
         });
 
